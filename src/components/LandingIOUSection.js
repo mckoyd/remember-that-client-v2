a33c09af373c4a1cec30329f519da42b4b7c3cca
mckoyd <dominick.mckoy@gmail.com>
2018-06-10 12:34:39 -0400

    Replaced banner and tightened responsive design

--- src/components/LandingIOUSection.js
+++ src/components/LandingIOUSection.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import '../styles/landing-iou-section.css';
 
+const IOUImage = require('../images/landing-iou-image.jpg');
 export const LandingIOUSection = () => (
   <article className='landing-section'>
-    <img className='landing-image' src={'https://compeap.com/wp-content/uploads/remember.jpg'} alt='Post-it note with `remember` written on it' /> 
+    <img className='landing-image' src={IOUImage} alt='Guy holding a dollar close up' /> 
     <div className='landing-iou'>
       <header>
         <h2>Remember IOU'S!</h2>
@@ -11,8 +12,8 @@ export const LandingIOUSection = () => (
       </header>
       <p>
       Keep track of who you owe, who owes you, and important receipts...<br />
-      Sign up above...<br/>
-      Already a member? Login and checkout your dashboard...
+      Want to check it out...sign up <span>here</span><br/>
+      Already a member? <span>Login</span> and checkout your dashboard...
       </p>
     </div>
   </article>
