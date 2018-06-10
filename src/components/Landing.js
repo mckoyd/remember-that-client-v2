38caf03152f5df424e1b0839a876e247ed1a719d
mckoyd <dominick.mckoy@gmail.com>
2018-06-10 03:01:44 -0400

    Added header to landing page

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { LandingTopNav } from './LandingTopNav';
 import LoginForm from './LandingLoginForm';
+import { LandingHeader } from './LandingHeader';
 
 export const Landing = () => (
   <div role='main'>
     <LandingTopNav />
     <LoginForm />
+    <LandingHeader />
   </div>
 );
---
+++ src/components/LandingHeader.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import '../styles/landing-header.css';
+
+export const LandingHeader = () => (
+  <header className='landing-header'>
+    <hgroup>
+      <h1>Remember THAT!</h1>
+      <h2>keep track of what matters MOST...</h2>
+    </hgroup>
+  </header>
+);
